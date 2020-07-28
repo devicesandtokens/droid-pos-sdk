6da7c1be2db67003a66702d54ef1220d0665c667
Chidalu17 <34123523+Chidalu17@users.noreply.github.com>
2020-07-28 19:41:58 +0100

    pinblock on kimono for telpo solved

--- smart-pos-emv-telpo/src/main/java/com/interswitch/smartpos/emv/telpo/utils/DataProcessUtil.java
+++ smart-pos-emv-telpo/src/main/java/com/interswitch/smartpos/emv/telpo/utils/DataProcessUtil.java
@@ -1,4 +1,5 @@
 package com.interswitch.smartpos.emv.telpo.utils;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -9,37 +10,33 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 /**
- * 单位：广东天波信息技术股份有限公�? 功能：数据处理组�? 创建人：luyq 日志�?2014-3-8
+ * 单位：广东天波信息技术股份有限公司 功能：数据处理组件 创建人：luyq 日志：2014-3-8
  */
 public class DataProcessUtil {
 
-    private static String hexStr =  "0123456789ABCDEF";
-    private static String[] binaryArray =
-            {"0000","0001","0010","0011",
-                    "0100","0101","0110","0111",
-                    "1000","1001","1010","1011",
-                    "1100","1101","1110","1111"};
+    private static String hexStr = "0123456789ABCDEF";
+    private static String[] binaryArray = {"0000", "0001", "0010", "0011",
+            "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011",
+            "1100", "1101", "1110", "1111"};
 
-    public static byte[] intToByteArray(int i){
+    public static byte[] intToByteArray(int i) {
         byte[] result = new byte[4];
-        result[0] = (byte)((i >> 24) & 0xFF);
-        result[1] = (byte)((i >> 16) & 0xFF);
-        result[2] = (byte)((i >> 8) & 0xFF);
-        result[3] = (byte)(i & 0xFF);
+        result[0] = (byte) ((i >> 24) & 0xFF);
+        result[1] = (byte) ((i >> 16) & 0xFF);
+        result[2] = (byte) ((i >> 8) & 0xFF);
+        result[3] = (byte) (i & 0xFF);
         return result;
     }
 
-
-
     /**
-	 * 字节数组转换为整�?
-	 * 
-	 * @param b
-	 * @return
-	 */
+     * 字节数组转换为整型
+     *
+     * @param b
+     * @return
+     */
     public static int byte2Int(byte[] b) {
         int intValue = 0;
-//        int tempValue = 0xFF;
+        // int tempValue = 0xFF;
         for (int i = 0; i < b.length; i++) {
             intValue += (b[i] & 0xFF) << (8 * (3 - i));
         }
@@ -47,27 +44,26 @@ public class DataProcessUtil {
     }
 
     /**
-	 * 整型转字节数�?
-	 * 
-	 * @param intValue
-	 * @return
-	 */
-    public static byte[] int2Byte(int intValue,int arrayLen) {
+     * 整型转字节数组
+     *
+     * @param intValue
+     * @return
+     */
+    public static byte[] int2Byte(int intValue, int arrayLen) {
         byte[] b = new byte[arrayLen];
         for (int i = 0; i < arrayLen; i++) {
-            b[i] = (byte) (intValue >> 8 * (arrayLen-1 - i) & 0xFF);
+            b[i] = (byte) (intValue >> 8 * (arrayLen - 1 - i) & 0xFF);
         }
         return b;
     }
 
-
     /**
-	 * 字节数组转十六进制字符串
-	 * 
-	 * @param b
-	 * @return
-	 * @throws Exception
-	 */
+     * 字节数组转十六进制字符串
+     *
+     * @param b
+     * @return
+     * @throws Exception
+     */
     public static String getHexString(byte[] b, int len) throws Exception {
         String result = "";
         for (int i = 0; i < len; i++) {
@@ -77,66 +73,65 @@ public class DataProcessUtil {
     }
 
     /**
-	 * 字符串转16进制字节数组
-	 * 
-	 * @param // hexString
-	 * @return
-	 */
-    public static byte[] getByteArray(String src){
-        byte[] res = new byte[src.length()/2];
+     * 字符串转16进制字节数组
+     *
+     * @param // hexString
+     * @return
+     */
+    public static byte[] getByteArray(String src) {
+        byte[] res = new byte[src.length() / 2];
         char[] chs = src.toCharArray();
         int[] b = new int[2];
 
-        for(int i=0,c=0; i<chs.length; i+=2,c++){
-            for(int j=0; j<2; j++){
-                if(chs[i+j]>='0' && chs[i+j]<='9'){
-                    b[j] = (chs[i+j]-'0');
-                }else if(chs[i+j]>='A' && chs[i+j]<='F'){
-                    b[j] = (chs[i+j]-'A'+10);
-                }else if(chs[i+j]>='a' && chs[i+j]<='f'){
-                    b[j] = (chs[i+j]-'a'+10);
+        for (int i = 0, c = 0; i < chs.length; i += 2, c++) {
+            for (int j = 0; j < 2; j++) {
+                if (chs[i + j] >= '0' && chs[i + j] <= '9') {
+                    b[j] = (chs[i + j] - '0');
+                } else if (chs[i + j] >= 'A' && chs[i + j] <= 'F') {
+                    b[j] = (chs[i + j] - 'A' + 10);
+                } else if (chs[i + j] >= 'a' && chs[i + j] <= 'f') {
+                    b[j] = (chs[i + j] - 'a' + 10);
                 }
             }
 
-            b[0] = (b[0]&0x0f)<<4;
-            b[1] = (b[1]&0x0f);
+            b[0] = (b[0] & 0x0f) << 4;
+            b[1] = (b[1] & 0x0f);
             res[c] = (byte) (b[0] | b[1]);
         }
 
         return res;
     }
 
-//    /**
-	// * 字符串转16进制字节数组
-//     *
-//     * @param hexString
-//     * @return
-//     */
-//    public static byte[] getByteArray(String hexString) {
-//        if(hexString==null)
-//            return null;
-//        int length=hexString.length()/2;
-//        if(hexString.length()%2!=0)
-//        {
-//            length+=1;
-//        }
-//        byte[] result=new byte[length];
-//        byte[] temp= new BigInteger(hexString, 16).toByteArray();
-//        System.arraycopy(temp,0,result,length-temp.length,temp.length);
-//        return result;
-//    }
-
+    // /**
+    // * 字符串转16进制字节数组
+    // *
+    // * @param hexString
+    // * @return
+    // */
+    // public static byte[] getByteArray(String hexString) {
+    // if(hexString==null)
+    // return null;
+    // int length=hexString.length()/2;
+    // if(hexString.length()%2!=0)
+    // {
+    // length+=1;
+    // }
+    // byte[] result=new byte[length];
+    // byte[] temp= new BigInteger(hexString, 16).toByteArray();
+    // System.arraycopy(temp,0,result,length-temp.length,temp.length);
+    // return result;
+    // }
 
     /**
-	 * �?16进制字符串转换成字节数组
-	 * 
-	 * @param hex
-	 * @return
-	 */
+     * 把16进制字符串转换成字节数组
+     *
+     * @param hex
+     * @return
+     */
     public static byte[] hexStringToByte(String hex) {
         int len = (hex.length() / 2);
         byte[] result = new byte[len];
-        char[] achar = hex.toCharArray();
+        char[] achar = hex.toUpperCase().toCharArray();
         for (int i = 0; i < len; i++) {
             int pos = i * 2;
             result[i] = (byte) (toByte(achar[pos]) << 4 | toByte(achar[pos + 1]));
@@ -149,13 +144,13 @@ public class DataProcessUtil {
         return b;
     }
 
-/** */
+    /** */
     /**
-	 * 把字节数组转换成16进制字符�?
-	 * 
-	 * @param bArray
-	 * @return
-	 */
+     * 把字节数组转换成16进制字符串
+     *
+     * @param bArray
+     * @return
+     */
     public static final String bytesToHexString(byte[] bArray) {
         StringBuffer sb = new StringBuffer(bArray.length);
         String sTemp;
@@ -168,16 +163,17 @@ public class DataProcessUtil {
         return sb.toString();
     }
 
-/** */
+    /** */
     /**
-	 * 把字节数组转换为对象
-	 * 
-	 * @param bytes
-	 * @return
-	 * @throws IOException
-	 * @throws ClassNotFoundException
-	 */
-    public static final Object bytesToObject(byte[] bytes) throws IOException, ClassNotFoundException {
+     * 把字节数组转换为对象
+     *
+     * @param bytes
+     * @return
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public static final Object bytesToObject(byte[] bytes) throws IOException,
+            ClassNotFoundException {
         ByteArrayInputStream in = new ByteArrayInputStream(bytes);
         ObjectInputStream oi = new ObjectInputStream(in);
         Object o = oi.readObject();
@@ -185,14 +181,14 @@ public class DataProcessUtil {
         return o;
     }
 
-/** */
+    /** */
     /**
-	 * 把可序列化对象转换成字节数组
-	 * 
-	 * @param s
-	 * @return
-	 * @throws IOException
-	 */
+     * 把可序列化对象转换成字节数组
+     *
+     * @param s
+     * @return
+     * @throws IOException
+     */
     public static final byte[] objectToBytes(Object s) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ObjectOutputStream ot = new ObjectOutputStream(out);
@@ -202,20 +198,22 @@ public class DataProcessUtil {
         return out.toByteArray();
     }
 
-    public static final String objectToHexString(Serializable s) throws IOException {
+    public static final String objectToHexString(Serializable s)
+            throws IOException {
         return bytesToHexString(objectToBytes(s));
     }
 
-    public static final Object hexStringToObject(String hex) throws IOException, ClassNotFoundException {
+    public static final Object hexStringToObject(String hex)
+            throws IOException, ClassNotFoundException {
         return bytesToObject(hexStringToByte(hex));
     }
 
-/** */
+    /** */
     /**
-	 * @函数功能: BCD码转�?10进制�?(阿拉伯数�?)
-	 * @输入参数: BCD�?
-	 * @输出结果: 10进制�?
-	 */
+     * @函数功能: BCD码转为10进制串(阿拉伯数据)
+     * @输入参数: BCD码
+     * @输出结果: 10进制串
+     */
     public static String bcd2Str(byte[] bytes) {
         StringBuffer temp = new StringBuffer(bytes.length * 2);
 
@@ -223,15 +221,16 @@ public class DataProcessUtil {
             temp.append((byte) ((bytes[i] & 0xf0) >>> 4));
             temp.append((byte) (bytes[i] & 0x0f));
         }
-        return temp.toString().substring(0, 1).equalsIgnoreCase("0") ? temp.toString().substring(1) : temp.toString();
+        return temp.toString().substring(0, 1).equalsIgnoreCase("0") ? temp
+                .toString().substring(1) : temp.toString();
     }
 
-/** */
+    /** */
     /**
-	 * @函数功能: 10进制串转为BCD�?
-	 * @输入参数: 10进制�?
-	 * @输出结果: BCD�?
-	 */
+     * @函数功能: 10进制串转为BCD码
+     * @输入参数: 10进制串
+     * @输出结果: BCD码
+     */
     public static byte[] str2Bcd(String asc) {
         int len = asc.length();
         int mod = len % 2;
@@ -241,12 +240,12 @@ public class DataProcessUtil {
             len = asc.length();
         }
 
-        byte abt[] = new byte[len];
+        byte[] abt = new byte[len];
         if (len >= 2) {
             len = len / 2;
         }
 
-        byte bbt[] = new byte[len];
+        byte[] bbt = new byte[len];
         abt = asc.getBytes();
         int j, k;
 
@@ -273,51 +272,52 @@ public class DataProcessUtil {
         }
         return bbt;
     }
-///** *//**
-	// * @函数功能: BCD码转ASC�?
-	// * @输入参数: BCD�?
-	// * @输出结果: ASC�?
-//     */
-//    public static String BCD2ASC(byte[] bytes) {
-//        StringBuffer temp = new StringBuffer(bytes.length * 2);
-//
-//        for (int i = 0; i < bytes.length; i++) {
-//            int h = ((bytes[i] & 0xf0) >>> 4);
-//            int l = (bytes[i] & 0x0f);
-//            temp.append(BToA[h]).append( BToA[l]);
-//        }
-//        return temp.toString() ;
-//    }
 
-/** */
+    // /** *//**
+    // * @函数功能: BCD码转ASC码
+    // * @输入参数: BCD串
+    // * @输出结果: ASC码
+    // */
+    // public static String BCD2ASC(byte[] bytes) {
+    // StringBuffer temp = new StringBuffer(bytes.length * 2);
+    //
+    // for (int i = 0; i < bytes.length; i++) {
+    // int h = ((bytes[i] & 0xf0) >>> 4);
+    // int l = (bytes[i] & 0x0f);
+    // temp.append(BToA[h]).append( BToA[l]);
+    // }
+    // return temp.toString() ;
+    // }
+
+    /** */
     /**
-	 * MD5加密字符串，返回加密后的16进制字符�?
-	 * 
-	 * @param origin
-	 * @return
-	 */
+     * MD5加密字符串，返回加密后的16进制字符串
+     *
+     * @param origin
+     * @return
+     */
     public static String MD5EncodeToHex(String origin) {
         return bytesToHexString(MD5Encode(origin));
     }
 
-/** */
+    /** */
     /**
-	 * MD5加密字符串，返回加密后的字节数组
-	 * 
-	 * @param origin
-	 * @return
-	 */
+     * MD5加密字符串，返回加密后的字节数组
+     *
+     * @param origin
+     * @return
+     */
     public static byte[] MD5Encode(String origin) {
         return MD5Encode(origin.getBytes());
     }
 
-/** */
+    /** */
     /**
-	 * MD5加密字节数组，返回加密后的字节数�?
-	 * 
-	 * @param bytes
-	 * @return
-	 */
+     * MD5加密字节数组，返回加密后的字节数组
+     *
+     * @param bytes
+     * @return
+     */
     public static byte[] MD5Encode(byte[] bytes) {
         MessageDigest md = null;
         try {
@@ -329,77 +329,73 @@ public class DataProcessUtil {
         }
     }
 
-
     /**
-	 * 
-	 * @param bArray
-	 * @return 转换为二进制字符�?
-	 */
-    public static String bytes2BinaryStr(byte[] bArray){
+     * @param bArray
+     * @return 转换为二进制字符串
+     */
+    public static String bytes2BinaryStr(byte[] bArray) {
 
         String outStr = "";
         int pos = 0;
-        for(byte b:bArray){
-			// 高四�?
-            pos = (b&0xF0)>>4;
-            outStr+=binaryArray[pos];
-			// 低四�?
-            pos=b&0x0F;
-            outStr+=binaryArray[pos];
+        for (byte b : bArray) {
+            // 高四位
+            pos = (b & 0xF0) >> 4;
+            outStr += binaryArray[pos];
+            // 低四位
+            pos = b & 0x0F;
+            outStr += binaryArray[pos];
         }
         return outStr;
 
     }
-    
-	/**
-	 * 
-	 * @param bytes
-	 * @return 将二进制转换为十六进制字符输�?
-	 */
-    public static String BinaryToHexString(byte[] bytes){
+
+    /**
+     * @param bytes
+     * @return 将二进制转换为十六进制字符输出
+     */
+    public static String BinaryToHexString(byte[] bytes) {
 
         String result = "";
         String hex = "";
-        for(int i=0;i<bytes.length;i++){
-			// 字节�?4�?
-            hex = String.valueOf(hexStr.charAt((bytes[i]&0xF0)>>4));
-			// 字节�?4�?
-            hex += String.valueOf(hexStr.charAt(bytes[i]&0x0F));
-            result +=hex+" ";
+        for (int i = 0; i < bytes.length; i++) {
+            // 字节高4位
+            hex = String.valueOf(hexStr.charAt((bytes[i] & 0xF0) >> 4));
+            // 字节低4位
+            hex += String.valueOf(hexStr.charAt(bytes[i] & 0x0F));
+            result += hex + " ";
         }
         return result;
     }
-    
-	/**
-	 * 
-	 * @param hexString
-	 * @return 将十六进制转换为字节数组
-	 */
-    public static byte[] HexStringToBinary(String hexString){
-		// hexString的长度对2取整，作为bytes的长�?
-        int len = hexString.length()/2;
-        byte[] bytes = new byte[len];
-		byte high = 0;// 字节高四�?
-		byte low = 0;// 字节低四�?
 
-        for(int i=0;i<len;i++){
-			// 右移四位得到高位
-            high = (byte)((hexStr.indexOf(hexString.charAt(2*i)))<<4);
-            low = (byte)hexStr.indexOf(hexString.charAt(2*i+1));
-			bytes[i] = (byte) (high | low);// 高地位做或运�?
+    /**
+     * @param hexString
+     * @return 将十六进制转换为字节数组
+     */
+    public static byte[] HexStringToBinary(String hexString) {
+        // hexString的长度对2取整，作为bytes的长度
+        int len = hexString.length() / 2;
+        byte[] bytes = new byte[len];
+        byte high = 0;// 字节高四位
+        byte low = 0;// 字节低四位
+
+        for (int i = 0; i < len; i++) {
+            // 右移四位得到高位
+            high = (byte) ((hexStr.indexOf(hexString.charAt(2 * i))) << 4);
+            low = (byte) hexStr.indexOf(hexString.charAt(2 * i + 1));
+            bytes[i] = (byte) (high | low);// 高地位做或运算
         }
         return bytes;
     }
 
     /**
-	 * 
-	 * @param str
-	 * @return 判断字符串是否全部为数字
-	 */
-    public static boolean isNumeric(String str){
-        for (int i = 0; i < str.length(); i++){
-            System.out.println(str.charAt(i));
-            if (!Character.isDigit(str.charAt(i))){
+     *
+     * @param str
+     * @return 判断字符串是否全部为数字
+     */
+    public static boolean isNumeric(String str) {
+        for (int i = 0; i < str.length(); i++) {
+//			System.out.println(str.charAt(i));
+            if (!Character.isDigit(str.charAt(i))) {
                 return false;
             }
         }
